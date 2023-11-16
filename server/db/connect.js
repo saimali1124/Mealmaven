9564ddd4184f8ccf5701aee2120359fb3ac5e8db
Saim <muhammadsaimali@gmail.com>
2023-11-16 16:07:08 +0500

    Resolved

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express')
 const app = express();
 
-//const uri= "mongodb+srv://saimali:brotherhood@cluster0.etcwjlk.mongodb.net/MealMaven?retryWrites=true&w=majority";
-const uri = "mongodb://127.0.0.1:27017/local";
+const uri= "mongodb+srv://saimali:brotherhood@cluster0.etcwjlk.mongodb.net/MealMaven?retryWrites=true&w=majority";
+// const uri = "mongodb://127.0.0.1:27017/local";
 
 mongoose.connect(uri, {
     
